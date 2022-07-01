29c80b3281ea5e203699cce8fa5e8d7ef62e6f8d
holusojivhictor <holusojivhictor@gmail.com>
2022-07-01 21:43:24 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/morpheus/tithe_box/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.morpheus.tithe_box
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tithe_box/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tithe box',
+    );
+  }
+}
